6ef3e7bb4c644235f7b1713cdb2d226fc64bc1d2
Broderick Bentil <bentilfrederick4@gmail.com>
2023-08-03 22:02:05 +0000

    Database Changes

--- backend/add_to-cart.php
+++ backend/add_to-cart.php
@@ -1,24 +1,43 @@
 <?php
 
-// Replace these with your database credentials
+// Requiring connection from connection.php file
 require('connection.php');
 
-// Replace these credentials with your database connection details
-// $host = 'localhost';
-// $dbname = 'ecommerce_db';
-// $username = 'your_username';
-// $password = 'your_password';
+// API endpoint to add an item to the cart
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if ($_POST['action'] === 'add_to_cart') {
+        $wig_ID = $_POST['$wig_ID'];
+        $wig_quantity = $_POST['$wig_quantity'];
+        $unit_price = $_POST['$unit_price'];
+        $total_price = $_POST['$total_price'];
+        $cart_status = 'open';
 
-// try {
-//     // Connect to the database
-//     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// } catch (PDOException $e) {
-//     die("Connection failed: " . $e->getMessage());
-// }
+        try {
+            $stmt = $conn->prepare("INSERT INTO carts (wig_ID, product_quantity, unit_price, total_price, cart_status)
+                                VALUES ( :wig_id, :product_quantity, :unit_price, :total_price, :cart_status)");
+
+            $stmt->bind_Param(':wig_ID', $wig_id);
+            $stmt->bind_Param(':product_quantity', $wig_quantity);
+            $stmt->bind_Param(':unit_price', $unit_price);
+            $stmt->bind_Param(':total_price', $total_price);
+            $stmt->bind_Param(':cart_status', $cart_status);
+
+            $stmt->execute();
+            echo json_encode(['message' => true]);
+        } catch (\Throwable $th) {
+            echo json_encode(['error' => False]);
+        }
+    }
+}
+    //Closed connection
+    $conn = null;
+
+
+    
+
 
 // Function to add an item to the cart
-function addToCart($wig_id, $wig_quantity, $unit_price, $total_price, $cart_status)
+/* function addToCart($wig_id, $wig_quantity, $unit_price, $total_price, $cart_status)
 {
     global $conn;
     try {
@@ -33,14 +52,14 @@ function addToCart($wig_id, $wig_quantity, $unit_price, $total_price, $cart_stat
 
         $stmt->execute();
         return true;
-    } catch (PDOException $e) {
+    } catch (PDOException $e) { */
         // Handle any errors here
-        return false;
+       /*  return false;
     }
-}
+} */
 
 // Function to retrieve all cart items for a specific cart
-function getCartItemsByUser($cart_id)
+/* function getCartItemsByUser($cart_id)
 {
     global $conn;
     try {
@@ -52,10 +71,10 @@ function getCartItemsByUser($cart_id)
         // Handle any errors here
         return [];
     }
-}
+} */
 
 // Function to update cart item quantity
-function updateCartItemQuantity($cart_id, $new_quantity)
+/* function updateCartItemQuantity($cart_id, $new_quantity)
 {
     global $conn;
     try {
@@ -64,14 +83,14 @@ function updateCartItemQuantity($cart_id, $new_quantity)
         $stmt->bindParam(':cart_id', $cart_id);
         $stmt->execute();
         return true;
-    } catch (PDOException $e) {
+    } catch (PDOException $e) { */
         // Handle any errors here
-        return false;
+       /*  return false;
     }
-}
+} */
 
 // Function to remove an item from the cart
-function removeFromCart($cart_id)
+/* function removeFromCart($cart_id)
 {
     global $conn;
     try {
@@ -79,13 +98,15 @@ function removeFromCart($cart_id)
         $stmt->bindParam(':cart_id', $cart_id);
         $stmt->execute();
         return true;
-    } catch (PDOException $e) {
+    } catch (PDOException $e) { */
         // Handle any errors here
-        return false;
+      /*   return false;
     }
-}
+} */
+    
+
 
 // Close the database connection
-$conn = null;
+/* $conn = null; */
 
 ?>
